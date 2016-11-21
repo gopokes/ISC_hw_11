2c47ab3667f26d0d1dbaf3347939663234f5d6c1
Bryan Rutter <Bryan Rutter>
2016-11-21 11:34:45 -0600

    corrected code for meso_chords; mesonet site locations are now plotting correctly...
    added code to check extent of various vector objects, and assign a global crs to each vector for correct layering

--- hw_11b_script.R
+++ hw_11b_script.R
@@ -21,7 +21,7 @@ meso_locs<-data.frame(meso_locs%>%#uses package:dplyr to filter decomissioned me
 summary(meso_locs)#summarizes filtered mesonet site location data
 county_borders_crs<-crs(county_borders)
 colnames(meso_locs)
-meso_coords<-meso_locs[,c(6,7)]#creates object for mesonet locations (lat,lon); used to converting to spatial dataframe
+meso_coords<-meso_locs[,c(7,6)]#creates object for mesonet locations (lat,lon); used to converting to spatial dataframe
 my_spatial_df<-SpatialPointsDataFrame(meso_coords,meso_locs,proj4string = county_borders_crs)#creates spatial points data frame from active mesonet locations and county_borders crs
 plot(my_spatial_df)
 #Exercise 3: Manipulating and Transforming Spatial Data
@@ -29,6 +29,16 @@ extent(county_borders)
 extent(NOAA_clim)
 extent(nat_ele)
 extent(my_spatial_df)
-levels(as.factor(NOAA_clim$STATE_CODE))
-levels(as.factor(NOAA_clim$ST_ABBRV))
-subset()
+OK_clim<-subset(NOAA_clim,ST_ABBRV="OK",select=c(OBJECTID,STATE,STATE_FIPS,CD_2DIG,STATE_CODE,CLIMDIV,CD_NEW,FIPS_CD,NCDC_GEO_I,NAME,ST_ABBRV,SHAPE_AREA,SHAPE_LEN))
+extent(OK_clim)
+crs(county_borders)
+crs(OK_clim)
+crs(nat_ele)
+crs(my_spatial_df)
+global_crs<-crs(county_borders)
+spTransform(OK_clim,global_crs)
+spTransform(my_spatial_df,global_crs)
+crs(nat_ele,proj4string=global_crs)
+crs(OK_clim)
+crs(nat_ele)
+crs(my_spatial_df)
